9e09d50e9acaf836ffbc4f8032e6ba9a7df66c73
Jason Guo <jsg353@cornell.edu>
2021-06-17 16:38:00 -0400

    First real commit

---
+++ simulations/simulation.py
@@ -0,0 +1,94 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import RK45, solve_ivp
+
+
+def B_wire(r, r0, I):
+    """
+    [r] is field observation point (2D vector)
+    [r0] is position of infinite wire (2D vector)
+    [I] is current (positive means in +z-direction)
+    Gives result in nanotesla
+    """
+    dist_vec = r - r0
+    z_hat = np.array([0, 0, 1])
+    direction = np.cross(z_hat, dist_vec)
+    B_hat = direction / np.linalg.norm(direction)
+    mu2pi = 2e+2  # mu_0 / 2pi (approx) # FIXME: Need to scale this
+    dist = np.linalg.norm(dist_vec)
+    return mu2pi * I / dist * B_hat
+
+
+def B_tot(r, wires):
+    """
+    [r] is field observation point (2D vector)
+    [wires] is array of (r0, I) pairs, one per infinite wire
+    Gives result in nanotesla
+    """
+    result = np.zeros(3)
+    for r0, I in wires:
+        result += B_wire(r, r0, I)
+    return result
+
+
+def B_two_wires(r, d, I1, I2):
+    """
+    Convention: y = 0 is the midline between the two wires
+    and (x, y) = (0, 0) is the midpoint between the two wires.
+    [r] is field observation point (2D vector)
+    [d] is the distance between wires
+    [I1], [I2] is current of top and bottom wire respectively
+    Gives result in nanotesla
+    """
+    top_wire_pos = np.array([0, d/2])
+    wires = [(top_wire_pos, I1), (-top_wire_pos, I2)]
+    return B_tot(r, wires)
+
+
+def func(v, d, I1, I2, t, S):
+    """
+    [t] is time
+    [S] is spin vector
+    [v] is speed
+    [d], [I1], [I2] same as in B_two_wires
+    """
+    x = v * t  # t = 0 corresponds to origin
+    c = 2 * -9.162e-2  # FIXME: justify value
+    return np.cross(c*S, B_two_wires([x, 0], d, I1, I2))
+
+
+# r = np.array([1, 4])
+# o = np.array([1, 2])
+
+# print(B_wire(r, o, 5))
+# print(B_tot(r, [(o, 5)]))
+# print(B_two_wires([0, 0], 10, 5, -5))
+
+v = 1000
+d = 10
+I1 = 10
+I2 = -10
+
+
+def f(t, S): return func(v, d, I1, I2, t, S)
+
+
+def naive(f, t_bounds, y0):
+    t0, tf = t_bounds
+    y = np.array(y0)
+    range, step = np.linspace(t0, tf, 100000, retstep=True)
+    for t in range:
+        y += step * f(t, y)
+    return y
+
+
+# range = np.linspace(-100, 100, 1000)
+# By = [B_two_wires([x, 0], d, I1, I2)[0] for x in range]
+# plt.plot(range, By)
+# plt.show()
+
+sol = solve_ivp(f, [-100, 100], [0, 1/2, 0], method="LSODA")
+print(sol.t)
+print(sol.y)
+
+print(naive(f, [-100, 100], [0, 1/2, 0]))
