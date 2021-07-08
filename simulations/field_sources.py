6e780aeca3ea52307eafc0646a468631bc04acd9
Jason Guo <jsg353@cornell.edu>
2021-07-07 23:33:32 -0400

    Add field sources file, add python gitignore stuff

---
+++ simulations/field_sources.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class FieldSource(ABC):
+    @abstractmethod
+    def field(self, r):
+        pass
+
+
+class InfiniteWire(FieldSource):
+    def __init__(self, p, I, R):
+        self.p = np.array(p)
+        self.I = np.array(I)
+        self.R = R
+
+    def dist_vec(self, r):
+        n = self.I / np.linalg.norm(self.I)
+        x = r - self.p
+        return x - n * np.dot(x, n)
+
+    def field(self, r):
+        dist_vec = self.dist_vec(r)
+        direction = np.cross(self.I, dist_vec)
+        B_hat = direction / np.linalg.norm(direction)
+        mu_over_2pi = 2e+2  # mu_0 / 2pi (approx), yields result in nanotesla
+        dist = np.linalg.norm(dist_vec)
+        I_norm = np.linalg.norm(self.I)
+        if dist < self.R:
+            return mu_over_2pi * I_norm * dist / (self.R * self.R) * B_hat
+        else:
+            return mu_over_2pi * I_norm / dist * B_hat
