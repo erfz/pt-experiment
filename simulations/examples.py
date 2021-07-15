feb668559e269cf1f031091dca02915606b477fd
Jason Guo <jsg353@cornell.edu>
2021-07-15 15:16:18 -0400

    Update examples.py

--- simulations/examples.py
+++ simulations/examples.py
@@ -4,25 +4,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def vlad_probability(S):
+def sim_vlad_probability(S):
     return 100 * (S[2] + 1 / 2)
 
 
+def theory_vlad_probability(Hx, H_dot):
+    return np.exp(-np.pi * Hx * Hx / H_dot * -0.09162) * 100
+
+
 # %%
-Sf_two_wires = Particle(
+Sf, ts, spins = Particle(
     [1000, 0, 0],
     np.zeros(3),
     generate_two_wires(10, (10, 0), (-10, 0)),
     [-100, 100],
     [0, 1 / 2, 0],
-).simulate()
-print(f"Final S (two wires): {Sf_two_wires}")
+).simulate_with_output()
+print(f"Final S (two wires): {Sf}")
+plt.plot(ts, list(zip(*spins)), label=("S_x", "S_y", "S_z"))
+plt.xlabel("Time (s)")
+plt.ylabel("Spin components")
+plt.legend()
+plt.show()
 # %%
-Sf_vlad = Particle(
-    np.empty(3), np.empty(3), [Vladimirskii(10, -41.5)], [-10, 10], [0, 0, 1 / 2]
-).simulate()
-print(f"Final S (Vladimirskii): {Sf_vlad}")
-print(f"-> Corresponding realignment probability: {vlad_probability(Sf_vlad)}%")
+Hx = 10
+H_dot = -41.5
+Sf, ts, spins = Particle(
+    np.empty(3), np.empty(3), [Vladimirskii(Hx, H_dot)], [-10, 10], [0, 0, 1 / 2]
+).simulate_with_output()
+print(f"Final S (Vladimirskii): {Sf}")
+print(f"Corresponding realignment probability: {sim_vlad_probability(Sf)}%")
+print(f"Theoretical realignment probability: {theory_vlad_probability(Hx, H_dot)}%")
+plt.plot(ts, list(zip(*spins)), label=("S_x", "S_y", "S_z"))
+plt.xlabel("Time (s)")
+plt.ylabel("Spin components")
+plt.legend()
+plt.show()
 # %%
 Sf_rand_line = run_two_wires_line(1000, 10, (10, 0), (-10, 0), 100, [-100, 100])
 print(f"Final S (rand line): {Sf_rand_line}")
@@ -34,7 +51,7 @@ print(f"Final S ({shape}): {Sf_shape}")
 Hx = 10
 H_dots = np.linspace(-10, -200, 20)
 sim_ps = [
-    vlad_probability(
+    sim_vlad_probability(
         Particle(
             np.empty(3),
             np.empty(3),
@@ -46,7 +63,7 @@ sim_ps = [
     for H_dot in H_dots
 ]
 
-vlad_ps = [np.exp(-np.pi * Hx * Hx / H_dot * -0.09162) * 100 for H_dot in H_dots]
+vlad_ps = [theory_vlad_probability(Hx, H_dot) for H_dot in H_dots]
 
 plt.plot(H_dots, sim_ps, label="Simulated")
 plt.plot(H_dots, vlad_ps, label="Vladimirskii prediction")
@@ -69,3 +86,5 @@ plt.xlabel("Time (s)")
 plt.ylabel("Spin components")
 plt.legend()
 plt.show()
+
+# %%
